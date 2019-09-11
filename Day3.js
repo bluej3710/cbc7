ba768c59eaf286b566de2f2c0f236c6e84a325da
Jeremy <bluej3710@gmail.com>
2019-09-11 15:27:10 -0400

    execise

---
+++ Day3.js
@@ -0,0 +1,20 @@
+var counter = 2;
+
+counter = counter+5; 
+console.log(counter);
+//7
+counter = counter*7;
+console.log(counter);
+//49 
+counter = counter % 3; 
+console.log(counter);
+//1 
+counter = counter % 2; 
+//I think the return will be 2 14 4*3=12 left over will be 2
+console.log(counter);
+//0
+ 
+//I think the return will be 1 14 7*2=14 left over will be 1
+
+//prob 2
+
