103562db8aa93dd172f66bce25e425ade9fa78ff
Jeremy <bluej3710@gmail.com>
2019-09-19 15:28:42 -0400

    Day9jshtmlclock/auto

---
+++ Day9Autocomplet.js
@@ -0,0 +1,12 @@
+//Js
+//An Array containing all the Pe {
+var Pets = document.forms["cats","dogs","penguins","parrots"].value;
+// if (x == "") {
+//   alert("Name must be filled out");
+//   return false;
+}
+} 
+myObj = {name: "cats", "dogs", "penguins","parrots"};
+myJSON = JSON.stri(myObngifyj);
+localStorage.setItem("testJSON", myJSON);
+}
